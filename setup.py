507ea944ebad1e1af5729fd03bcf64bbada81b77
uwe <uwe.krien@rl-institut.de>
2018-06-19 19:49:09 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(name='my_reegis',
+      version='0.0.1',
+      author='Uwe Krien',
+      author_email='uwe.krien@rl-institut.de',
+      description='A reegis heat and power model of Berlin.',
+      package_dir={'my_reegis': 'my_reegis'},
+      install_requires=['oemof >= 0.1.0',
+                        'pandas >= 0.17.0']
+      )
