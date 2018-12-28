eaded9c8c84500d26a226b55213eb3353e91830b
uwe <uvchik.git@posteo.eu>
2018-12-28 16:01:23 +0100

    Add time analysis to snippets

--- my_reegis/snippets.py
+++ my_reegis/snippets.py
@@ -14,6 +14,45 @@ from oemof.tools import logger
 import reegis_tools.geometries as geometries
 
 
+def time_analysis():
+    df = pd.read_csv('/home/uwe/log_time_test.csv', parse_dates=True)
+    print(df.index)
+    idx = pd.MultiIndex(levels=[[], [], []], labels=[[], [], []])
+    time_results = pd.DataFrame(index=idx)
+    df['t_value'] = df.index
+    df['delta'] = (df['t_value']-df['t_value'].shift()).fillna(0)
+    del df['ms']
+    del df['t_value']
+    del df['log']
+    df = df[df.module != 'main']
+    del df['module']
+    for row in df.iterrows():
+        elapsed_time = row[1].delta
+        msg = row[1].msg.replace('.esys.', '').split(os.sep)
+        name = msg[-1]
+        name_split = name.split('_')
+        try:
+            year = int(name_split[1])
+            base = name_split[0]
+            var = '_'.join(name_split[2:])
+        except ValueError:
+            year = int(name_split[2])
+            base = '_'.join(name_split[:2])
+            var = '_'.join(name_split[3:])
+        solver = msg[-2].split('_')[-1]
+        col = 'time_{0}'.format(solver)
+        time_results.loc[(year, base, var), col] = (
+            elapsed_time)
+        time_results.sort_index(inplace=True)
+    time_results['diff'] = (
+            time_results['time_cbc'].abs() - time_results['time_gurobi'].abs())
+    print(time_results.groupby(level=[0, 1]).sum())
+    print(time_results.groupby(level=[0, 1]).sum().sum())
+    time_results['Prz'] = time_results['diff'].div(time_results.time_cbc)
+    time_results.to_excel('/home/uwe/time_test.xls')
+    time_results.to_csv('/home/uwe/time_test.csv')
+
+
 def convert_shp2csv(infile, outfile, index_col='region'):
     logging.info("Converting {0} to {1}.".format(infile, outfile))
     df = geometries.load(fullname=infile, index_col=index_col)
@@ -65,6 +104,8 @@ def create_grid_for_deflex_regions():
 
 if __name__ == "__main__":
     logger.define_logging()
+    time_analysis()
+    exit(0)
     p = '/home/uwe/chiba/Promotion/reegis_geometries/regions_states'
     inf = os.path.join(p, 'de17_state_no_tso.shp')
     outf = os.path.join(p, 'csv', 'de17_state_no_tso.csv')
