a10fbcf54df19b64d28790d018c70bbf2361d2e9
uvchik <uvchik.git@posteo.eu>
2020-11-06 15:59:08 +0100

    Add requirement

--- setup.py
+++ setup.py
@@ -2,13 +2,22 @@
 
 from setuptools import setup
 
-setup(name='my_reegis',
-      version='0.0.1',
-      author='Uwe Krien',
-      author_email='uwe.krien@rl-institut.de',
-      description='A reegis heat and power model of Berlin.',
-      package_dir={'my_reegis': 'my_reegis'},
-      install_requires=['oemof.solph >= 0.1.0',
-                        'pandas >= 0.17.0',
-                        'matplotlib', 'numpy', 'reegis', 'deflex', 'geopandas']
-      )
+setup(
+    name="my_reegis",
+    version="0.0.1",
+    author="Uwe Krien",
+    author_email="uwe.krien@rl-institut.de",
+    description="A reegis heat and power model of Berlin.",
+    package_dir={"my_reegis": "my_reegis"},
+    install_requires=[
+        "oemof.solph >= 0.1.0",
+        "pandas >= 0.17.0",
+        "requests",
+        "matplotlib",
+        "numpy",
+        "reegis",
+        "deflex",
+        "geopandas",
+    ],
+    extras_require={"dummy": ["oemof"]}
+)
