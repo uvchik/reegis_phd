ddc3c4732ee2046db10efc4239724e8786e71228
uvchik <uvchik.git@posteo.eu>
2020-11-13 13:34:40 +0100

    Set requirements to fix version

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 
 from setuptools import setup
 
+
+github = "@https://github.com/"
 setup(
     name="my_reegis",
     version="0.0.1",
@@ -10,16 +12,22 @@ setup(
     description="A reegis heat and power model of Berlin.",
     package_dir={"my_reegis": "my_reegis"},
     install_requires=[
-        "oemof.solph >= 0.1.0",
-        "pandas >= 0.17.0",
-        "requests",
-        "matplotlib",
-        "demandlib",
-        "pytz",
-        "numpy",
-        "reegis",
-        "deflex",
-        "geopandas",
+        "oemof.solph == 0.4.1",
+        "pandas == 1.1.4",
+        "requests == 2.25",
+        "matplotlib == 3.3.3",
+        "demandlib{0}oemof/demandlib/archive/v0.1.7b1.zip".format(github),
+        "pytz == 2020.4",
+        "numpy == 1.19.4",
+        "reegis{0}reegis/reegis/archive/phd.zip".format(github),
+        "deflex{0}reegis/deflex/archive/phd.zip".format(github),
+        "geopandas == 0.8.1",
     ],
-    extras_require={"dummy": ["oemof"]}
+    extras_require={"dummy": ["oemof"]},
+    entry_points={
+        "console_scripts": [
+            "phd_figures = reegis_phd.figures.figures:main",
+            "reproduce_phd_optimisation = reegis_phd.reproduce:main",
+        ]
+    },
 )
